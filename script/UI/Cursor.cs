6f3c314553749cf06807a6cf9fa91a89e1ef1e00
GulpeIT <grishawork-01@mail.ru>
2024-10-10 14:02:09 +0300

    ver 0.0.1y

--- script/UI/Cursor.cs
+++ script/UI/Cursor.cs
@@ -4,16 +4,16 @@ using System;
 public partial class Cursor : Node2D
 {   
 
-    private Sprite2D _CursorSprite;
+    private Sprite2D CURSOR_SPRITE;
 
     public override void _Ready()
     {
         base._Ready();
         
         Input.SetMouseMode(Input.MouseModeEnum.Hidden);
-        _CursorSprite = GetNode<Sprite2D>("Sprite2D");
+        CURSOR_SPRITE = GetNode<Sprite2D>("Sprite2D");
 
-        _CursorSprite.Frame = 0;
+        CURSOR_SPRITE.Frame = 0;
     }
 
     public override void _Process(double delta)
@@ -27,11 +27,13 @@ public partial class Cursor : Node2D
         base._Input(@event);
         if (@event.IsActionPressed("ui_mouse_button_left_click"))
         {
-            _CursorSprite.Position = Vector2.Down;
+            CURSOR_SPRITE.Position = Vector2.Down;
+            CURSOR_SPRITE.Frame = 2;
         }
         else if (@event.IsActionReleased("ui_mouse_button_left_click"))
         {
-            _CursorSprite.Position = Vector2.Zero;
+            CURSOR_SPRITE.Position = Vector2.Zero;
+            CURSOR_SPRITE.Frame = 1;
         }
     }
 }
