e9f4656131056cd2fd9a69e93cbfe9101aa4a2de
GulpeIT <grishawork-01@mail.ru>
2024-10-10 15:40:20 +0300

    version 0.0.1u

--- script/game_object/Camera.cs
+++ script/game_object/Camera.cs
@@ -1,18 +1,40 @@
 using Godot;
-using System;
 
 public partial class Camera : Camera2D
 {
+	
+	// Изменение позиции камеры по определённым точка на сцене
+	[ExportCategory("Передвижение камеры по точкам")]
 	[Export] 
-	Marker2D[] POSITION  = new Marker2D[] {};
+	private Marker2D[] MARKERS  = new Marker2D[] {};
+	[Export(PropertyHint.Range, "0, 999")]
+	public int CURRENT_POSITION = 0;
 
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	float MOVE_TIME = 0.75f;
+
 	public override void _Ready()
 	{
+		MoveToPoint(2);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	/// <summary>
+	/// Method call to change camera position a new point
+	/// </summary>
+	/// <param name="newPoint">
+	/// This is points in the array "MARKERS"
+	/// </param>
+	public void MoveToPoint(int newPoint)
 	{
+		if (newPoint > MARKERS.Length)
+		{
+			GD.PrintErr($"Camera: Method MoveToPoint get value outweigh marks length");
+			return;
+		}
+		CURRENT_POSITION = newPoint;
+		Tween tween = GetTree().CreateTween();
+		tween.SetTrans(Tween.TransitionType.Cubic);
+		tween.SetEase(Tween.EaseType.Out);
+		tween.TweenProperty(GetNode("../Camera"), "position", MARKERS[CURRENT_POSITION].Position, MOVE_TIME);
 	}
 }
